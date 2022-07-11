ed0bbb6ecc7ddc398b4482424f8b97a6fc6edbdb
LuQuintas11 <luciaquintas14@gmail.com>
2022-07-11 23:12:42 +0000

    sds

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -4,9 +4,22 @@
  */
 
 // No spread operator
+let arr1 = [1,2,3];
+let arr2= arr1;
+arr2.push(4)
+console.log("Second array;", arr2)
 
+//to remain untouch the first array
 // Copying an array
+let arr3 = [2,3,4];
+let arr4=[...arr3];
+
+arr4.push(6)
 
 // Copying an object
+let obj1 = { a: 1, b: 2, c: 3};
+let obj2= {...obj1, d:4};
+
 
 // Copying only part of an array/object
+let arr5=[...arr1, {...arr1{...obj1}, ...arr3 "x","y", "z"];
